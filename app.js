d5c2ad95e1ed11663c541a1589de47ff36a9e54e
Dominik <46494682+ludzik940@users.noreply.github.com>
2022-03-16 10:38:13 +0100

    First upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById('open').style.display = 'none';
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById('open').style.display = 'block';
+  }
