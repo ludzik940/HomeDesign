1cb42f0865db06e0e36da1181d8a6380c74bbdc1
Dominik <46494682+ludzik940@users.noreply.github.com>
2022-03-18 20:11:13 +0100

    Updating styles and pages

--- app.js
+++ app.js
@@ -5,3 +5,19 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+
+
+  const showContentOne = () => {
+    let text = document.getElementById('content-one');
+    let dots = document.getElementById('dots-one')
+    dots.classList.toggle('content-hide');
+    text.classList.toggle('content-show');
+  };
+
+  const showContentTwo = () => {
+    let text = document.getElementById('content-two');
+    let dots = document.getElementById('dots-two')
+    dots.classList.toggle('content-hide');
+    text.classList.toggle('content-show');
+  };
+
